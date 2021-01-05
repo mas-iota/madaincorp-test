8080ca942a494c9a2f2bb27e08355222a46df159
Mahmoud Al-Sharif <mahmoud.k.alsharif@gmail.com>
2021-01-05 04:09:15 +0200

    feat(app): added js files

---
+++ js/app.js
@@ -0,0 +1,26 @@
+import { random } from './util.js'
+
+// 1- click on rock / paper / scissors
+
+// fn play()
+ // 1- random computer option
+ // 2- reflect on document what computer chose
+ // 3- evaluate both options
+ //   a- if equal then draw
+ //   b- if user rock
+ //     i- if computer paper computer WIN
+ //     ii- if computer scissors user WIN
+ //   b- if user paper
+ //     i- if computer scissors computer WIN
+ //     ii- if computer rock user WIN
+ //   b- if user scissors
+ //     i- if computer rock computer WIN
+ //     ii- if computer paper user WIN
+ // 4- log the result
+
+
+// fn log()
+ // 1- add score
+ // 2- reflect on document
+
+// fn helper.random()
