fca2652a5dddc7b781060630eb5a0a765c874841
Mahmoud Al-Sharif <mahmoud.k.alsharif@gmail.com>
2021-01-05 04:40:11 +0200

    feat(app): play function / logic

--- js/app.js
+++ js/app.js
@@ -1,4 +1,52 @@
-import { random } from './util.js'
+import { getRandomOption } from './util.js'
+
+let playerCtrlBtns = document.querySelectorAll('.player-controls button')
+playerCtrlBtns.forEach((btn) => btn.addEventListener('click', play))
+
+const score = { computer: 0, player: 0 }
+
+function play(e) {
+  const playerOption = e.target.dataset.type
+  const computerOption = getRandomOption()
+  let status
+
+  document.querySelector('#player-option').innerText = playerOption
+  document.querySelector('#computer-option').innerText = computerOption
+
+  if (playerOption === computerOption) {
+    status = 'Tie!'
+  } else if (playerOption === 'rock') {
+    if (computerOption === 'paper') {
+      // c win
+      status = 'Lost!'
+    } else if (computerOption === 'scissors') {
+      // u win
+      status = 'Won!'
+    }
+  } else if (playerOption === 'paper') {
+    if (computerOption === 'scissors') {
+      // c win
+      status = 'Lost!'
+
+    } else if (computerOption === 'rock') {
+      // u win
+      status = 'Won!'
+
+    }
+  } else if (playerOption === 'scissors') {
+    if (computerOption === 'rock') {
+      // c win
+      status = 'Lost!'
+
+    } else if (computerOption === 'paper') {
+      // u win
+      status = 'Won!'
+
+    }
+  }
+
+  document.querySelector('#round-status').innerText = status
+}
 
 // 1- click on rock / paper / scissors
 
--- js/util.js
+++ js/util.js
@@ -1,2 +1,5 @@
-export function random() {
+const options = ['rock', 'paper', 'scissors']
+
+export function getRandomOption() {
+  return options[Math.floor(Math.random() * (2 + 1))]
 }
